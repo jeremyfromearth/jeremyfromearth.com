a2eecb1919c4c61976e8960c4edb50d05e75e9ef
jeremyfromearth <jeremyfromearth@gmail.com>
2018-02-03 10:10:11 -0800

    Added supporting files for uibot

---
+++ assets/js/app/waves.js
@@ -0,0 +1,15 @@
+define(["module"], function (module) {
+  "use strict";
+
+  function _classCallCheck(instance, Constructor) {
+    if (!(instance instanceof Constructor)) {
+      throw new TypeError("Cannot call a class as a function");
+    }
+  }
+
+  var Waves = function Waves() {
+    _classCallCheck(this, Waves);
+  };
+
+  module.exports = Waves;
+});
