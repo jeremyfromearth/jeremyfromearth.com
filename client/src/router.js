0076ed83f5fb6cc8c6331a780a1a1675b397d76e
jeremyfromearth <jeremynealbrown@gmail.com>
2019-02-07 17:44:17 -0800

    Renamed HelloWorld to Main and added Vuex store

--- client/src/router.js
+++ client/src/router.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from './components/HelloWorld'
+import Main from './components/Main'
 
 Vue.use(Router);
 
@@ -9,7 +9,7 @@ export default new Router({
     {
       path: '/',
       name: 'home',
-      component: HelloWorld,
+      component: Main,
     }
   ],
 });
