384001bd305ceb006fda9d4c263886b6a3b2d4d0
jeremyfromearth <jeremynealbrown@gmail.com>
2019-02-09 19:08:17 -0800

    Added animation to Navigation input text

--- client/src/components/Navigation.vue
+++ client/src/components/Navigation.vue
@@ -2,10 +2,9 @@
 export default {
   name: 'Navigation',
   data() {
-    const init = this.$router.history.current.path.split('/')[1]
     return {
-      input: init,
-      current_path: init,
+      input: this.$router.history.current.path.split('/')[1],
+      interval_id: -1,
       paths: [
         {label: '/main', path: '/', exact: true},
         {label: '/about', path: '/about', exact: true},
@@ -17,21 +16,44 @@ export default {
     }
   },
   computed: {
-    get_input_string() {
+    get_full_input_string() {
       return '~/' + this.input + '_'
+    }
+  },
+  methods: {
+    animate_input_text(i) {
+      let direction = 0
+      let new_input = i.split('/')[1]
+      if(new_input == 'main') new_input = ''
+
+      clearInterval(this.interval_id)
+      this.interval_id = setInterval(()=> {
+        if(direction == 0) {
+          if(this.input.length > 0) {
+            this.input = this.input.slice(0, this.input.length-1)
+          } else { 
+            direction = 1
+          }
+        } else {
+          if(this.input.length < new_input.length) {
+            this.input += new_input[this.input.length]
+          } else {
+            clearInterval(this.interval_id)
+          }
+        }
+      }, 50)
     }
   },
   watch: {
     $route(to) {
-      this.current_path = to.path.split('/')[1]
-      this.input = this.current_path == 'main' ? '' : `${this.current_path}`
+      this.animate_input_text(to.path) 
     }
   }
 }
 </script>
 <template>
   <div>
-    <h1>{{ get_input_string }}</h1>
+    <h1>{{ get_full_input_string }}</h1>
     <div class='nav-links'>
       <router-link 
         v-for='(path, idx) in paths' 
